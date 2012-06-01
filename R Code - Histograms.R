30b54a4cb607c0929d3a3b85dfa2fde488ffc924
James Wong <jcwong86@gmail.com>
2012-06-01 09:10:26 -0400

    Added initial files.

---
+++ R Code - Histograms.R
@@ -0,0 +1,95 @@
+setwd("~/James Docs")
+wd <- getwd()
+list.files(wd)
+cabi <- read.csv(file="bikeBatchOut-test.csv", header=TRUE)
+
+#Return the whole row of the highest TransitTime
+cabi[cabi$TransitTime == max(cabi$TransitTime, na.rm=TRUE),]
+
+#View a subset of columns of data
+#How do I select columns 3, 6-8 and 11, for example?
+View(subset(cabi,select=WalkTime:PlannedBikeDist))
+
+#Avoids retyping data table
+with(cabi,plot(CaBiTripTime,TransitTime))
+
+#Add Columns calculated from previous columns
+cabi = transform(cabi, TotalTransitTime = WalkTime+TransitTime+WaitingTime)
+cabi = transform(cabi, 
+                 CaBiTime_mins=CaBiTripTime/60, 
+                 TotalTransitTime_mins=TotalTransitTime/60, 
+                 WalkTime_mins=WalkTime/60, 
+                 TransitTime_mins=TransitTime/60,
+                 WaitingTime_mins=WaitingTime/60,
+                 PlannedBikeTime_mins=PlannedBikeTime/60, 
+                 TotalTransitTripDist=TotalTransitTripDist/1609.344,
+                 PlannedBikeDist=PlannedBikeDist/1609.344)
+cabi = transform(cabi, BikeTimeDiff_percent = (PlannedBikeTime_mins-CaBiTime_mins)/PlannedBikeTime_mins)
+cabi = transform(cabi, BikeTimeDiff_abs = (PlannedBikeTime_mins-CaBiTime_mins))
+
+#View a subset of columns to store
+#is there a faster way to create a view/subset of specific columns?
+keep <- c("CaBiTripTime","TotalTransitTripDist","PlannedBikeTime","PlannedBikeDist","CaBiTime_mins","TotalTransitTime_mins","WalkTime_mins","TransitTime_mins","WaitingTime_mins","PlannedBikeTime_mins","BikeTimeDiff_percent")
+View(subset(cabi,select=keep))
+
+#Plot a Histogram about comparing planned and actual bike travel times (percentages)
+bins=c(-5000, -2.5, -2.4, -2.3, -2.2, -2.1, -2, -1.9, -1.8, -1.7, -1.6, -1.5, -1.4, -1.3, -1.2, -1.1, -1, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2, 2.1, 2.2, 2.3, 2.4, 2.5, 5000)
+hist(cabi$BikeTimeDiff_percent,
+     breaks=bins, 
+     freq=TRUE,
+     xlim=c(-2.2,2.2),
+     xlab="Percent Travel Time Difference between Planned and CaBi Trips",
+     main="Comparison of planned and actual bike travel times between CaBi stations",
+     col=rgb(100,100,100,75,maxColorValue=255))
+
+#A beautiful histogram
+hist(cabi$CaBiTime_mins,
+     breaks=500, 
+     freq=TRUE,
+     xlim=c(0,60),
+     xlab="Length of CaBi Trips (min)",
+     main="Distributon of CaBi Trip Times",
+     col=rgb(152,30,50,200,maxColorValue=255),
+     border=FALSE,
+     lab=c(5,10),
+     xaxt="n",
+     yaxt="n")
+axis(2,at=100*(0:20),las=2)
+axis(2,at=100*(0:20),tck=1,col="white",lwd=2,labels=FALSE)
+axis(1,at=10*(0:6), tck=1,col="white",lwd=5,labels=FALSE)
+axis(1,at=10*(0:6))
+
+
+#Practice scatterplot
+
+with(cabi,
+     plot(
+       CaBiTime_mins,
+       PlannedBikeTime_mins,
+       xlim=c(0,45),
+       ylim=c(0,45),
+       ylab="CaBi Trip Time (min)",
+       xlab="OTP Transit Time (min)",
+       main="CaBi Trip Time as a function of Predicted Bike Travel Time",
+       col=rgb(100,100,100,25,maxColorValue=255),
+       pch=16,
+       asp=1,
+       abline(a=0,b=1)
+     )
+)
+
+with(cabi,
+     plot(
+       TotalTransitTime_mins,
+       CaBiTime_mins,
+       xlim=c(0,45),
+       ylim=c(0,45),
+       ylab="CaBi Trip Time (min)",
+       xlab="OTP Transit Time (min)",
+       main="CaBi Trip Time as a function of Predicted Bike Travel Time",
+       col=rgb(100,100,100,45,maxColorValue=255),
+       pch=16,
+       asp=1,
+       abline(a=0,b=1)
+     )
+)
---
+++ batch.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python
+
+# -------------------------------------------------------
+#	Batch O-D Processing Script for OpenTripPlanner
+#	Written by:	Andrew Byrd (abyrd@openplans.org) and
+#               James Wong (jcwong86@gmail.com)
+#	Last Updated: 	May 2012
+#
+#	Useful links: 	www.opentripplanner.org/api-doc
+#					www.openplans.org
+#
+#--------------------------------------------------------
+
+import urllib, urllib2, json, csv					#import libraries
+from datetime import date, datetime, timedelta
+from time import sleep, strftime
+
+INPUT = "cabi.csv"									#Designate Input filename and path
+OUTPUT = "bikeBatchOut-test.csv"					#Designate Output filename and path
+DECIMATE = 100										#Script samples every Nth record
+URL = 'http://analyst.opentripplanner.org/opentripplanner-api-webapp/ws/plan?'
+													#URL and PLAN api from OTP deployment
+SLEEP = 1 											#Seconds between api calls
+LIMIT = 3600 										#Number of total executed requests
+	
+weekday = date.today().weekday() 					#Current day of the week
+monday = date.today() - timedelta(days=weekday) 	#Num of days away from nearest Monday
+
+# Universal linefeed reader for mac/win text files
+outputFile = open(OUTPUT, 'w') 						#Overwrite a file
+writer = csv.writer(outputFile, dialect='excel')
+	#Format for .csv
+writer.writerow(['rowNum','CaBiTripTime','StartDate','StartTime','StartLat','StartLon','EndLat','EndLon','WalkTime', 'TransitTime','WaitingTime','NumTransfers','TotalTransitTripDist','PlannedBikeTime','PlannedBikeDist'])
+													#Write header row in .csv file
+
+# Takes CSV input, creates URLs, stores data locally in row array
+with open(INPUT, 'rU') as inputFile : 				#Continue using file
+    reader = csv.reader(inputFile, dialect='excel')	#Read the input .csv file
+    rowNum = 1
+    for row in reader :								#Samples file based on DECIMATE
+        rowNum += 1
+
+        if rowNum >LIMIT*DECIMATE :
+            break
+        if rowNum % DECIMATE != 0 :
+            continue
+        print "Row ", rowNum
+        try :
+            										#assign input values directly into row
+            duration, date, time, o_lat, o_lon, d_lat, d_lon = row 
+            										# [t(f) for t, f in zip(types2, row)]
+            #Sets date to current equivalent weekday for use with up-to-date GTFS
+            date_new = datetime.strptime(date, "%m/%d/%Y")
+            date_new = monday + timedelta(days=date_new.weekday())
+            date_new = date_new.strftime("%m/%d/%Y")
+            row[1] = date_new            
+            time_new = datetime.strptime(time, "%I:%M:%S %p").strftime("%T")
+													#creates time object
+        except Exception as e :
+            print e
+            print 'cannot parse input record, skipping'
+            continue
+        arriveBy = False							#Using departure times for all calls
+        params =  {'time' : '%s' % time_new,		#Sets up the URL parameters
+                   'fromPlace' : '%s,%s' % (o_lat, o_lon),
+                   'toPlace' :   '%s,%s' % (d_lat, d_lon),
+                   'maxWalkDistance' : 2000,		#Arbitrary
+                   'mode' : 'WALK,TRANSIT', 		#See OTP API Documentation
+                   'date' : date_new,
+                   'numItineraries' : 1, 			#For simplicity, keep set at 1. Max=3
+                   'bannedRoutes' : 'Test_Purple',
+                   'arriveBy' : 'true' if arriveBy else 'false' }
+        # Tomcat server + spaces in URLs -> HTTP 505 confusion. urlencode percent-encodes parameters.
+        url = URL + urllib.urlencode(params)		#Create a POST link using params
+        req = urllib2.Request(url)
+        req.add_header('Accept', 'application/json')
+        print url
+        try :
+            response = urllib2.urlopen(req) 
+        except urllib2.HTTPError as e :
+            print e
+            continue
+        try :
+            content = response.read()				#Store response from OTP
+            objs = json.loads(content)
+            itineraries = objs['plan']['itineraries']
+            										#Access itinerary objects from content
+        except :
+            print 'no itineraries'
+            continue
+        for i in itineraries :						#Calc total dist from a multi-leg trip
+        	dist = 0
+        	if i['transitTime'] != 0 :
+        		for leg in i['legs'] :
+        			dist += leg['distance']
+        			
+		#Within each itinerary, get specific metrics and store them in the output row
+        for i in itineraries :						
+            outrow = [ rowNum ] + row + [ i['walkTime'], i['transitTime'], i['waitingTime'], i['transfers'], dist]
+
+
+		#Repeat API call to get a BIKE itinerary and store results
+        params =  {'time' : '%s' % time_new,
+                   'fromPlace' : '%s,%s' % (o_lat, o_lon),
+                   'toPlace' :   '%s,%s' % (d_lat, d_lon),
+                   'maxWalkDistance' : 2000,
+                   'mode' : 'BICYCLE', 				#Only diff is in mode
+                   'date' : date_new,
+                   'numItineraries' : 1, 
+                   'bannedRoutes' : 'Test_Purple',
+                   'arriveBy' : 'true' if arriveBy else 'false' }
+        url = URL + urllib.urlencode(params)
+        req = urllib2.Request(url)
+        req.add_header('Accept', 'application/json')
+        print url
+        try :
+            response = urllib2.urlopen(req) 
+        except urllib2.HTTPError as e :
+            print e
+            continue
+        try :
+            content = response.read()
+            objs = json.loads(content)
+            itineraries = objs['plan']['itineraries']
+        except :
+            print 'no itineraries'
+            continue
+            print outrow
+        for i in itineraries :						#Walktime=Biketime when mode=BICYCLE
+            outrow = outrow + [i['walkTime'],i['walkDistance']]
+        writer.writerow(outrow)						#Write full row to output file
+        sleep(SLEEP)								#Delay to maintain OTP performance
+
+            
